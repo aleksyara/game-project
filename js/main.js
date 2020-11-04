7e32762d07b59fa60fff34f09cfee8a8723379ac
Aleksei Yarantsev <aleksyara@Alekseis-MacBook-Pro.local>
2020-11-04 08:54:29 -0800

    basic game functionality was complited

---
+++ js/main.js
@@ -0,0 +1,221 @@
+/*----- constants -----*/
+const suits = ['s', 'c', 'd', 'h'];
+const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+
+// Build a 'master' deck of 'card' objects used to create shuffled decks
+const masterDeck = buildMasterDeck();
+// renderDeckInContainer(masterDeck, document.getElementById('master-deck-container'));
+
+
+let user = {
+ userTotal: 0,
+ userCards: [],
+ userBank: 0,
+ userName: ''
+};
+
+let dealer = {
+    dealerTotal: 0,
+    dealerCards: []
+};
+
+/*----- app's state (variables) -----*/
+
+// let shuffledDeck;
+// let winner;
+// gameStatisitcs = {
+//     handsPlayed: null,
+//     playerWins: null,
+//     playerLoses: null, 
+//     winnigIndex: null
+// }
+
+
+/*----- cached element references -----*/
+const elRefShuffledDeckContainer = document.getElementById('shuffled-deck-container');
+const elRefDealersCards = document.getElementById('dealers-cards');
+const elRefUsersCards = document.getElementById('users-cards');
+const elRefUserScore = document.getElementById('user-score');
+
+// const elRefHitButton = document.getElementsByClassName('hit');
+// const elRefStandButton = document.getElementsByClassName('stand');
+// const elRefBetButton = document.getElementsByClassName('bet');
+
+
+
+/*----- event listeners -----*/
+document.querySelector('.hit').addEventListener('click', hitHandler);
+document.querySelector('.stand').addEventListener('click', standHandler);
+// document.querySelector('button').addEventListener('click', renderShuffledDeck);
+
+// document.querySelector('input').addEventListener('submit', playerBank);
+
+
+
+
+/*----- functions -----*/
+init(); 
+
+// initialize our state when the app loads in the browser
+
+
+/////////////////////
+
+function startNewGame() {
+    console.log('start new game');
+    console.log('user: ', user);
+    // show money and user name in user object
+}
+
+function hitHandler() {
+    console.log('hit button click');
+    user.userCards.push(shuffledDeck.shift());
+    console.log('user: ', user);
+    renderUsersCards();
+    countAndRenderUsersScore();
+    setTimeout(function() {
+        checkForWinningOrLoosingHand();
+    }, 200);
+}
+
+function standHandler() {
+    console.log('stand button click');
+    let dealerScore = dealer.dealerCards[0].value + dealer.dealerCards[1].value;
+    let minScore = 17;
+    
+
+    let ctr = 0;
+    while (dealerScore <= minScore) {
+        let tempCard = shuffledDeck.shift();
+        dealer.dealerCards.push(tempCard);
+        dealerScore += tempCard.value;
+        
+        console.log('ctr: ', ctr);
+        
+        ctr++;
+    }
+
+    console.log('dealerScore: ', dealerScore);
+    renderDealersCards();
+
+    let userScore = 0;
+    user.userCards.forEach(function(elem) {
+        userScore += elem.value
+    });
+
+    setTimeout(function() {
+        if (userScore === dealerScore) {
+            alert('Its a draw!');
+        } else if (dealerScore > userScore && dealerScore < 22) {
+            alert('Dealer won!');
+        } else if (dealerScore < userScore && dealerScore < 22) {
+            alert('User won!');
+        } else {
+            alert('User won!');
+        }
+    }, 500);
+    console.log('dealer: ', dealer);
+
+}
+
+function checkForWinningOrLoosingHand() {
+    let userScore = 0;
+    let dealerScore = 0;
+    user.userCards.forEach(function(elem) {
+        userScore += elem.value
+    });
+    dealer.dealerCards.forEach(function(elem) {
+        dealerScore += elem.value
+    });
+    if (userScore === 21) {
+        alert('Black Jack, You won!');
+    } else if (userScore > 21) {
+        alert('You have lost your bet!');
+    }
+}
+
+function init () {
+    console.log('game firing');
+    renderShuffledDeck();
+    dealTwoCardsToUserAndDealer();
+    renderDealersCards();
+    renderUsersCards();
+    countAndRenderUsersScore();
+}
+
+function countAndRenderUsersScore() {
+    let sum = 0;
+    user.userCards.forEach(function(elem) {
+        sum += elem.value
+    });
+    elRefUserScore.innerHTML = 'Score: ' + sum;
+}
+
+
+function renderDealersCards() {
+    renderDeckInContainer(dealer.dealerCards, elRefDealersCards);
+}
+
+function renderUsersCards() {
+    renderDeckInContainer(user.userCards, elRefUsersCards);
+}
+
+function dealTwoCardsToUserAndDealer() {
+    //deler cards
+    dealer.dealerCards.push(shuffledDeck.shift());
+    dealer.dealerCards.push(shuffledDeck.shift());    
+    console.log('dealer: ', dealer);
+        
+    //user cards
+    user.userCards.push(shuffledDeck.shift());
+    user.userCards.push(shuffledDeck.shift());
+    console.log('user: ', user);
+}
+
+function renderShuffledDeck() {
+    // Create a copy of the masterDeck (leave masterDeck untouched!)
+    const tempDeck = [...masterDeck];
+    shuffledDeck = [];
+    while (tempDeck.length) {
+      // Get a random index for a card still in the tempDeck
+      const rndIdx = Math.floor(Math.random() * tempDeck.length);
+      // Note the [0] after splice - this is because splice always returns an array and we just want the card object in that array
+      shuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
+    }
+    renderDeckInContainer(shuffledDeck, elRefShuffledDeckContainer);
+  }
+  
+  function renderDeckInContainer(deck, container) {
+    container.innerHTML = '';
+    // Let's build the cards as a string of HTML
+    // Use reduce when you want to 'reduce' the array into a single thing - in this case a string of HTML markup 
+    const cardsHtml = deck.reduce(function(html, card) {
+      return html + `<div class="card ${card.face}"></div>`;
+    }, '');
+    container.innerHTML = cardsHtml;
+  }
+  
+  function buildMasterDeck() {
+    const deck = [];
+    // Use nested forEach to generate card objects
+    suits.forEach(function(suit) {
+      ranks.forEach(function(rank) {
+        deck.push({
+          // The 'face' property maps to the library's CSS classes for cards
+          face: `${suit}${rank}`,
+          // Setting the 'value' property for game of blackjack, not war
+          value: Number(rank) || (rank === 'A' ? 11 : 10)
+        });
+      });
+    });
+    return deck;
+  }
+  
+//   renderShuffledDeck();
+
+
+// take our state (which our the variables we initialized in our init function)
+// and update the dom with those values
+function render(){
+
+}
